8b48b09d54916befa73e5b99cfffda65883d3378
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-07-17 23:03:17 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1520 ms, Memory: 64144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input[] = br.readLine().split(" "); // 공백을 기준으로 나누기
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+//        String input[] = br.readLine().split(" "); // 공백을 기준으로 나누기
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         int A[] = new int[n];
-        String array[] = br.readLine().split(" ");
-        for (int i = 0; i < array.length; i++) {
-            A[i] = Integer.parseInt(array[i]);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
         int S[] = new int[n];
         S[0] = A[0];
@@ -21,9 +23,9 @@ public class Main {
         }
 
         for (int i =0; i < m; i++) { // M번만큼 반복
-            String section[] = br.readLine().split(" "); // 인덱스는 0번부터 시작하므로 -1
-            int start = Integer.parseInt(section[0]) - 1 ;
-            int end = Integer.parseInt(section[1]) - 1;
+            st = new StringTokenizer(br.readLine()); // 인덱스는 0번부터 시작하므로 -1
+            int start = Integer.parseInt(st.nextToken()) - 1 ;
+            int end = Integer.parseInt(st.nextToken()) - 1;
             if (start == 0) { // 첫 번째 수부터의 합 배열일 경우
                 System.out.println(S[end]);
             }
