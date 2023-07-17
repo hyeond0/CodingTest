78450f70b4d10c088f8ea413aa04e405f4f2f34a
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-07-17 22:41:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1772 ms, Memory: 68332 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int A[] = new int[N];
-        int S[] = new int[N];
-        for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-            A[i] = num;
-            if (i==0) {
-                S[i] = num;
-            }
-            else {
-                S[i] = S[i-1] + num;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = br.readLine().split(" "); // 공백을 기준으로 나누기
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        int A[] = new int[n];
+        String array[] = br.readLine().split(" ");
+        for (int i = 0; i < array.length; i++) {
+            A[i] = Integer.parseInt(array[i]);
+        }
+        int S[] = new int[n];
+        S[0] = A[0];
+        for (int i = 1; i < n; i++) {
+            S[i] = S[i - 1] + A[i];
         }
 
-        for (int i = 0; i < M; i++) {
-            int answer = 0;
-            int section1 = sc.nextInt();
-            int section2 = sc.nextInt();
-            if (section1 == 1) {
-                answer = S[section2-1];
+        for (int i =0; i < m; i++) { // M번만큼 반복
+            String section[] = br.readLine().split(" "); // 인덱스는 0번부터 시작하므로 -1
+            int start = Integer.parseInt(section[0]) - 1 ;
+            int end = Integer.parseInt(section[1]) - 1;
+            if (start == 0) { // 첫 번째 수부터의 합 배열일 경우
+                System.out.println(S[end]);
             }
             else {
-                answer = S[section2-1] - S[section1-2];
+                System.out.println(S[end] - S[start - 1]);
             }
-            System.out.println(answer);
         }
     }
 }
