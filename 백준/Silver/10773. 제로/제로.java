9e91a22d98ba4e556c87265b31b11e62888f9545
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-07-27 19:33:35 +0900

    [Silver IV] Title: 제로, Time: 820 ms, Memory: 115088 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0 ; i < K; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                stack.pop();
+            }
+            else {
+                stack.add(num);
+            }
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
