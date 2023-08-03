695008c32f34296e209924d881eb82a619f0311e
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-08-03 14:22:38 +0900

    [Gold V] Title: 탑, Time: 3448 ms, Memory: 155192 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -7,9 +7,10 @@ public class Main {
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
         Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
         Stack<Integer> index = new Stack<>();
         stack.add(Integer.parseInt(st.nextToken()));
-        System.out.print(0 + " ");
+        arrayList.add(0);
         index.add(1);
 
         for (int i = 1; i < n; i++) {
@@ -21,16 +22,20 @@ public class Main {
                     index.pop();
                 } else {
                     isNone = false;
-                    if (!index.isEmpty()) System.out.print(index.peek() + " ");
+                    if (!index.isEmpty()) arrayList.add(index.peek());
                     break;
                 }
             }
             if (isNone) {
-                System.out.print(0 + " ");
+                arrayList.add(0);
 
             }
             stack.add(num);
             index.add(i+1);
+        }
+
+        for (int i = 0; i< arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + " ");
         }
     }
 }
