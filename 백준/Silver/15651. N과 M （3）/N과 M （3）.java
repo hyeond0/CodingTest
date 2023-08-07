64acd5557bdc03dfe204b04a3b3a4cc1bbc0f2cc
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-08-08 02:17:04 +0900

    [Silver III] Title: N과 M (3), Time: 520 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected;
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            // 1-N 까지를 k번
+            for (int cand = 1; cand <= N; cand++) {
+                selected[k] = cand;
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        selected = new int[M+1];
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+}
