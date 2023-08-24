d99cc7d23700294935a0f98effc24abe2fe464a1
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-08-25 00:21:38 +0900

    [Gold III] Title: 소문난 칠공주, Time: 396 ms, Memory: 78588 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -2,14 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
-
     static String[] map = new String[5];
-
-    static int[][] dist = new int[5][5];
+    static int[][] choiceMap = new int[5][5];
     static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     static int[] selected = new int[8];
     static int answer;
@@ -20,13 +14,9 @@ public class Main {
         }
     }
     private static void ref_func(int k) {
-        if (k == 8) {
-//            if (selected[1] == 6 && selected[2] == 7 && selected[3] == 8 && selected[4] == 9 && selected[5] == 10 && selected[6] == 15 && selected[7] == 20) {
-//                int[] index = findIndex();
-//                bfs(index[0], index[1]);
-//            }
-            int[] index = findIndex();
-            bfs(index[0], index[1]);
+        if (k == 8) { // 1 - 25 중 7개를 뽑았다면
+            int[] index = findIndex(); // 7개 숫자를 map 인덱스로 찾아가 choiceMap에 선택 처리
+            bfs(index[0], index[1]); // 맨 처음 선택된 숫자로 bfs 탐색 시작
         } else {
             int start = selected[k - 1];
             for (int cand = start+1; cand <= 25; cand++) {
@@ -37,44 +27,44 @@ public class Main {
         }
 
     }
-
     private static void bfs(int x, int y) {
-        int countS = 0;
-        int count = 0;
+        int sCount = 0; // 7개 정점 중 이다솜파 카운트
+        int linkCount = 1; // 7개 정점이 간선으로 연결되어 있는지 카운트
+
         Queue<Integer> Q = new LinkedList<>();
         Q.add(x);
         Q.add(y);
-        dist[x][y] = 1;
-        if (map[x].charAt(y) == 'S') countS++;
-        count++;
+        choiceMap[x][y] = 1; // 뽑힌 7개 정점 중에서도 탐색을 한 정점은 1
+        if (map[x].charAt(y) == 'S') sCount++; // 이다솜파인지 확인
 
         while (!Q.isEmpty()) {
             x = Q.poll();
             y = Q.poll();
-            for (int k = 0; k < 4; k++) {
+            for (int k = 0; k < 4; k++) { // 격자형 그래프 간선 확인
                 int nx = x + dir[k][0];
                 int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
-                if (dist[nx][ny] != 0) continue; // 뽑은 7개가 아니면 continue
+                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue; // map 밖으로 나가는지
+                if (choiceMap[nx][ny] != 0) continue; // 뽑은 7개가 아니거나 이미 탐색을 했는지
                 Q.add(nx);
                 Q.add(ny);
-                count++;
-                dist[nx][ny] = 1;
-                if (map[nx].charAt(ny) == 'S') countS++;
+                linkCount++;
+                choiceMap[nx][ny] = 1; // 탐색 여부 체크
+                if (map[nx].charAt(ny) == 'S') sCount++; // 이다솜파인지 확인
             }
         }
-
-        if (count == 7 && countS >= 4) answer++;
+        // 탐색이 끝난 뒤 7개 정점이 연결되어있고, 이다솜파가 4명 이상이라면
+        if (linkCount == 7 && sCount >= 4) answer++;
     }
 
     private static int[] findIndex() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                dist[i][j] = -1; // dist 초기화
+                choiceMap[i][j] = -1; // choiceMap 초기화
             }
         }
 
         int[] ret = new int[2];
+        // 1 - 25 까지 중에서 뽑아온 7개를 2차원 배열 index에 맞게 저장.
         for (int i = 1; i <= 7; i++) {
             int x = selected[i] / 5;
             int y;
@@ -84,21 +74,19 @@ public class Main {
             }
             else y = selected[i] % 5 - 1;
             if (i == 1) {
-                ret[0] = x;
+                ret[0] = x; // bfs 탐색을 위해 7개 중 맨 처음 뽑힌 정점 좌표를 x, y로 저장
                 ret[1] = y;
-            } else dist[x][y] = 0;
+            }
+            choiceMap[x][y] = 0; // 뽑아온 7개는 0, 나머지는 -1로 탐색 시작
         }
         return ret;
     }
 
-    private static void pro() {
-        // dist 초기화
-
-        // 백트래킹을 통해 숫자 7개 뽑기 1 - 25까지
+    public static void main(String[] args) throws IOException {
+        input();
+        // 백트래킹을 통해 숫자 1 - 25 중 7개 뽑아서 bfs 돌리기
         ref_func(1);
-        // 7개를 뽑는다면 조건 확인 -> bfs 등등
-
-        // 경우의 수 출력
+        // 칠공주 결성 경우의 수 출력
         System.out.println(answer);
     }
 }
