21d40fe5dc71f280a70e1c9b4d2f82b9fc839b9c
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-09-01 23:11:17 +0900

    [Silver V] Title: 배열 합치기, Time: 3176 ms, Memory: 338952 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -5,23 +5,34 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
     static StringBuilder sb = new StringBuilder();
 
-
-    static int N, M;
-    static int[] A;
+    static int n, m;
+    static int[] a, b;
 
     static void input() {
-        N = scan.nextInt();
-        M = scan.nextInt();
-        A = new int[N + M + 1];
-        for (int i = 1; i <= N + M; i++) {
-            A[i] = scan.nextInt();
+        n = scan.nextInt();
+        m = scan.nextInt();
+        a = new int[n + 1];
+        b = new int[m + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
+        }
+        for (int i = 1; i <= m; i++) {
+            b[i] = scan.nextInt();
         }
     }
 
     private static void pro() {
-        Arrays.sort(A, 1, N + M + 1);
-        for (int i = 1; i <= N + M; i++) sb.append(A[i]).append(' ');
-        System.out.println(sb.toString());
+        int L = 1, R = 1;
+        // a와 b를 앞에서부터 하나씩 추출해서 출력한다. 단, 둘 다 비어있지 않은 경우와 그것이 아닌 경우를 잘 나누자.
+        /* TODO */
+        while (L <= n && R <= m) {
+            if (a[L] <= b[R]) sb.append(a[L++]).append(' ');
+            else sb.append(b[R++]).append(' ');
+        }
+        while (L <= n) sb.append(a[L++]).append(' ');
+        while (R <= m) sb.append(b[R++]).append(' ');
+
+        System.out.println(sb);
     }
 
     public static void main(String[] args) {
