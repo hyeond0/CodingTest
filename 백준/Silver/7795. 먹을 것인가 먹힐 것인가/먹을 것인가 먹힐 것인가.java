f7cef440ca78cfddbf3d2c5a136645d3881300aa
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-10-03 23:31:31 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1240 ms, Memory: 175884 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static Scanner scan = new Scanner(System.in);
+    static int N, M;
+    static int[] A, B;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new int[N + 1];
+        B = new int[M + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = scan.nextInt();
+        }
+        for (int i = 1; i <= M; i++) {
+            B[i] = scan.nextInt();
+        }
+    }
+
+    static int lower_bound(int[] A, int L, int R, int X) {
+        int res = L - 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (A[mid] < X) {
+                res = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    static void pro() {
+        Arrays.sort(B, 1, M + 1);
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            ans += lower_bound(B, 1, M, A[i]);
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        int TT;
+        TT = scan.nextInt();
+        for (int tt = 1; tt <= TT; tt++) {
+            input();
+            pro();
+        }
+    }
+}
