8a05824388eaf66345cc7453575858eb615d44e2
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2023-10-05 10:53:29 +0900

    [level 2] Title: 피로도, Time: 0.26 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,12 +1,12 @@
-import java.util.*;
 class Solution {
+
     int answer, dungeon;
     int[] selected;
     public int solution(int k, int[][] dungeons) {
         answer = -1;
         dungeon = dungeons.length;
         for (int i = 1; i <= dungeon; i++) {
-            selected = new int[i+1];
+            selected = new int[i];
             ref_func(0, k, dungeons, i);
         }
         return answer;
@@ -19,7 +19,9 @@ class Solution {
             for (int i = 0; i < select; i++) {
                 int minPiro = dungeons[selected[i]][0];
                 int usePiro = dungeons[selected[i]][1];
-                if (minPiro > presentPiro) return;
+                if (minPiro > presentPiro) {
+                    return;
+                }
                 presentPiro -= usePiro;
                 count++;
             }
@@ -27,12 +29,14 @@ class Solution {
         } else {
             for (int cand = 0; cand < dungeon; cand++) {
                 boolean isUsed = false;
-                for (int i=0;i<n;i++) {
-                    if (cand == selected[i]) isUsed = true;
+                for (int i = 0; i < n; i++) {
+                    if (cand == selected[i]) {
+                        isUsed = true;
+                    }
                 }
                 if (!isUsed) {
                     selected[n] = cand;
-                    ref_func(n+1, k, dungeons, select);
+                    ref_func(n + 1, k, dungeons, select);
                     selected[n] = 0;
                 }
             }
