645d6afc16b85772bd7e246f2ca3dcd1ac734fbb
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-01-01 20:44:47 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 259.48 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2 == 1){
+            System.out.println(n + " " + "is odd");
+        } else {
+            System.out.println(n + " " + "is even");
+        }
+    }
+}
