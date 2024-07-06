f312caa44769009e9cfecb50f9fde0f43d9fd700
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-06 23:14:06 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,12 +1,5 @@
-select 
-    ug.TITLE, 
-    ug.BOARD_ID, 
-    ur.REPLY_ID, 
-    ur.WRITER_ID, 
-    ur.CONTENTS, 
-    date_format(ur.CREATED_DATE, '%Y-%m-%d') CREATED_DATE
-from USED_GOODS_BOARD ug
-inner join used_goods_reply ur
-on ug.BOARD_ID = ur.BOARD_ID
-where date_format(ug.CREATED_DATE, '%Y-%m') = '2022-10'
-order by CREATED_DATE asc, title asc;
+SELECT BOARD_ID, REPLY_ID, WRITER_ID, CONTENTS, CREATED_DATE
+from USED_GOODS_REPLY ur
+join USED_GOODS_BOARD ub
+on ur.BOARD_ID = ub.BOARD_ID
+order by CREATED_DATE, ub.TITLE
