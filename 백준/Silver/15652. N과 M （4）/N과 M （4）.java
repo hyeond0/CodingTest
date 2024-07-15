5da70c9f2e2ff9399ee6a83c6cae7064d3f10875
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-15 10:45:04 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 18124 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,36 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] selected;
+	static int N, M;
+	static int[] selected;
+	static StringBuilder sb = new StringBuilder();
 
-    static void rec_func(int k) {
-        if (k == M + 1) {
-            for (int i = 1; i <= M; i++) {
-                sb.append(selected[i]).append(' ');
-            }
-            sb.append('\n');
-        } else {
-            // 1-N 까지를 k번
-            int start = selected[k-1];
-            if (start==0) start = 1;
-            for (int cand = start; cand <= N; cand++) {
-                selected[k] = cand;
-                rec_func(k + 1);
-                selected[k] = 0;
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		input();
+		rec_func(1);
+		System.out.println(sb.toString());
+	}
 
+	static void input() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		selected = new int[M + 1];
+	}
 
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        selected = new int[M+1];
-        rec_func(1);
-        System.out.println(sb.toString());
-    }
+	static void rec_func(int k) throws Exception {
+		if (k == M + 1) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(selected[i]).append(' ');
+			}
+			sb.append('\n');
+		} else {
+			int start = selected[k-1];
+			if (start == 0) start = 1;
+			for (int cand = start; cand <= N; cand++) {
+				selected[k] = cand;
+				rec_func(k + 1);
+				selected[k] = 0;
+			}
+		}
+	}
 }
