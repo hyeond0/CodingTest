28d6e53bed8ab5c2763f9b2bbd5e5c74e51d1db4
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-19 17:04:18 +0900

    [Silver III] Title: 두 수의 합, Time: 412 ms, Memory: 30708 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,44 +2,83 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
-    static int N;
-    static int[] A;
-    static void input() {
-        N = scan.nextInt();
-        A = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-    }
+	static int N, X, ans;
+	static int[] A;
+	static FastScanner scan = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
 
-    static boolean binary_search(int[] A, int L, int R, int X, int value) {
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (A[mid] + value == X) return true;
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-            if (A[mid] + value > X) {
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        return false;
-    }
-    static void pro() {
-        int X = scan.nextInt();
-        Arrays.sort(A, 1, N+1);
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
-            if (binary_search(A, i+1, N, X, A[i])) count++;
-        }
-        System.out.println(count);
-    }
+	static void input() {
+		N = scan.nextInt();
+		A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = scan.nextInt();
+		}
+		X = scan.nextInt();
+	}
 
-    public static void main(String[] args) {
+	static boolean bin_search(int[] A, int L, int R, int X) {
+		while (L <= R) {
+			int mid = (L + R) / 2;
+			if (A[mid] == X) {
+				return true;
+			}
 
-        input();
-        pro();
-    }
+			if (A[mid] < X) {
+				L = mid + 1;
+			} else {
+				R = mid - 1;
+			}
+		}
+		return false;
+	}
+
+	static void pro() {
+		Arrays.sort(A, 1, N + 1);
+		int ans = 0;
+		for (int i = 1; i <= N - 1; i++) {
+			if (bin_search(A, i+1, N, X - A[i])) {
+				ans++;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			try {
+				while (st == null || !st.hasMoreElements()) {
+					st = new StringTokenizer(br.readLine());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return st.nextToken();
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
