3467446312fcd2f482955a96b535bbc947215254
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-25 11:19:46 +0900

    [Silver II] Title: 예산, Time: 180 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,54 +1,95 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N,M;
-    static int[] A;
-    static Scanner scan = new Scanner(System.in);
-    static void input() {
-        N = scan.nextInt();
-        A = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-        M = scan.nextInt();
-    }
+	static int N, M;
+	static int[] A;
 
-    static boolean detemination(int budget) {
-        // 예산의 최댓값
-        int sum = 0;
-        for (int i = 1; i <= N; i++) {
-            if (A[i] >= budget) sum += budget;
-            else sum += A[i];
-        }
-        return sum <= M;
-    }
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
-    static void pro() {
-        // A 정렬
-        Arrays.sort(A, 1, N+1);
-        int L = 0, R = M, result = 0;
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if (detemination(mid)) {
-                result = mid;
-                L = mid + 1;
-            } else {
-                R = mid - 1;
-            }
-        }
-        // 최댓값 탐색
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, A[i]);
-        }
-        if (max <= result) System.out.println(max);
-        else System.out.println(result);
-    }
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
+	static void input() {
+		N = scan.nextInt();
+		A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = scan.nextInt();
+		}
+		M = scan.nextInt();
+	}
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+	static void pro() {
+		Arrays.sort(A, 1, N + 1);
+		int max_budget = possible_budget();
+		if (max_budget > -1) {
+			System.out.println(max_budget);
+			return;
+		}
+		int L = 0, R = 1000000000, ans = 0;
+		while (L <= R) {
+			int mid = (L + R) / 2;
+			if (determination(mid)) {
+				ans = mid;
+				L = mid + 1;
+			} else {
+				R = mid - 1;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	static int possible_budget() {
+		int sum = 0, max = 0;
+		for (int i = 1; i <= N; i++) {
+			sum += A[i];
+			if (A[i] > max) {
+				max = A[i];
+			}
+		}
+		if (sum <= M) {
+			return max;
+		} else {
+			return -1;
+		}
+	}
+
+	static boolean determination(int budget) {
+		int res = 0;
+		for (int i = 1; i <= N; i++) {
+			if (A[i] > budget) {
+				res += budget;
+			} else {
+				res += A[i];
+			}
+		}
+		return res <= M;
+	}
+
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
