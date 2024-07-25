2bfe77c87291f6b3277bb8665bcc624a36b1ad63
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-25 11:05:58 +0900

    [Silver II] Title: 랜선 자르기, Time: 240 ms, Memory: 18768 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,46 +1,76 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
+	static int K, N;
+	static int[] A;
 
-    static int K, N;
-    static int[] A;
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
-    static void input() {
-        K = scan.nextInt();
-        N = scan.nextInt();
-        A = new int[K + 1];
-        for (int i = 1; i <= K; i++) {
-            A[i] = scan.nextInt();
-        }
-    }
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-    static boolean determination(long H) {
-        long sum = 0;
-        for (int i = 1; i <= K; i++) {
-            sum += A[i] / H;
-        }
-        return sum >= N;
-    }
+	static void input() {
+		K = scan.nextInt();
+		N = scan.nextInt();
+		A = new int[K + 1];
+		for (int i = 1; i <= K; i++)
+			A[i] = scan.nextInt();
+	}
 
-    static void pro() {
-        long L = 1, R = (long) (Math.pow(2,31) - 1) * 10000, result = 0;
-        while (L <= R) {
-            long mid = (L + R) / 2;
-            if (determination(mid)) {
-                result = mid;
-                L = mid + 1;
-            } else {
-                R = mid - 1;
-            }
-        }
-        System.out.println(result);
-    }
+	static void pro() {
+		Arrays.sort(A, 1, K + 1);
+		long R = 1;
+		for (int i = 0; i < 32; i++) {
+			R *= 2;
+		}
+		R -= 1;
+		long L = 0, ans = 0;
+		while (L <= R) {
+			long mid = (L + R) / 2;
+			if (determination(mid)) {
+				ans = Math.max(ans, mid);
+				L = mid + 1;
+			} else {
+				R = mid - 1;
+			}
+		}
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+		System.out.println(ans);
+	}
+
+	static boolean determination(long H) {
+		long res = 0;
+		for (int i = 1; i <= K; i++) {
+			res += A[i] / H;
+		}
+		return res >= N;
+	}
+
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
