fd75567ad0f65701cc1c32c47e551f1ff1d083f2
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-26 11:10:00 +0900

    [Silver I] Title: 기타 레슨, Time: 360 ms, Memory: 24344 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,51 +1,79 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] A;
-    private static void input() {
-        Scanner scan = new Scanner(System.in);
-        N = scan.nextInt();
-        M = scan.nextInt();
-        A =new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-    }
+	static int N, M;
+	static int[] A;
 
-    private static boolean determination(int length) {
-        int[] B = new int[M+1];
-        int index = 1;
-        for (int i = 1; i <= N; i++) {
-            // 블루레이 크기보다 커진다면 다음 블루레이에 담기
-            if (B[index] + A[i] > length)
-                index++;
-            // 블루레이 갯수에 다 안담기면 이 길이로는 못 만듦
-            if (index > M) return false;
-            B[index] += A[i];
-        }
-        return true; // 잘 담기면 true
-    }
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
-    private static void pro() {
-        // 탐색
-        int L = 0, R = 1000000000, answer = 0;
-        for (int i = 1; i <= N; i++) L = Math.max(L, A[i]);
-        while (L <= R) {
-            int mid = (L + R) / 2;
-            if(determination(mid)) {
-                answer = mid;
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+	static void input() {
+		N = scan.nextInt();
+		M = scan.nextInt();
+		A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = scan.nextInt();
+		}
+	}
+
+	static void pro() {
+		int L = 1, R = 1000000000, ans = 0;
+		while (L <= R) {
+			int mid = (L + R) / 2;
+			if (determination(mid)) {
+				ans = mid;
+				R = mid - 1;
+			} else {
+				L = mid + 1;
+			}
+		}
+		System.out.println(ans);
+	}
+
+	static boolean determination(int minute) {
+		int current_minute = minute;
+		int blueray = 1;
+		for (int i = 1; i <= N; i++) {
+			if (current_minute - A[i] >= 0) {
+				current_minute -= A[i];
+			} else if (minute >= A[i]) {
+				current_minute = minute - A[i];
+				blueray++;
+			} else {
+				return false;
+			}
+		}
+
+		return blueray <= M;
+	}
+
+	static class FastScanner {
+		StringTokenizer st;
+		BufferedReader br;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
