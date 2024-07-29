10fa1a10156f1da56c0d1c5d46242f9bf650302a
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-29 10:56:51 +0900

    [Silver V] Title: 배열 합치기, Time: 1680 ms, Memory: 356484 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,42 +1,54 @@
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static List<Integer> a = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
-    static int n, m;
-    static int[] a, b;
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-    static void input() {
-        n = scan.nextInt();
-        m = scan.nextInt();
-        a = new int[n + 1];
-        b = new int[m + 1];
-        for (int i = 1; i <= n; i++) {
-            a[i] = scan.nextInt();
-        }
-        for (int i = 1; i <= m; i++) {
-            b[i] = scan.nextInt();
-        }
-    }
+	static void input() {
+		n = scan.nextInt();
+		m = scan.nextInt();
+		for (int i = 1; i <= n + m; i++) {
+			a.add(scan.nextInt());
+		}
+	}
 
-    private static void pro() {
-        int L = 1, R = 1;
-        // a와 b를 앞에서부터 하나씩 추출해서 출력한다. 단, 둘 다 비어있지 않은 경우와 그것이 아닌 경우를 잘 나누자.
-        /* TODO */
-        while (L <= n && R <= m) {
-            if (a[L] <= b[R]) sb.append(a[L++]).append(' ');
-            else sb.append(b[R++]).append(' ');
-        }
-        while (L <= n) sb.append(a[L++]).append(' ');
-        while (R <= m) sb.append(b[R++]).append(' ');
+	static void pro() {
+		Collections.sort(a);
+		for (int num : a) {
+			sb.append(num).append(' ');
+		}
+		System.out.println(sb.toString());
+	}
 
-        System.out.println(sb);
-    }
+	static class FastScanner {
+		StringTokenizer st;
+		BufferedReader br;
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
