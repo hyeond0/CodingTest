1a94bee876988f6dc091e4706d214c85c087b06b
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-29 10:48:43 +0900

    [Silver I] Title: 귀여운 라이언, Time: 448 ms, Memory: 73712 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,39 +1,67 @@
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
+	static int n, k;
+	static int[] a;
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-    static int N, K;
-    static int[] A;
+	static void input() {
+		n = scan.nextInt();
+		k = scan.nextInt();
+		a = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = scan.nextInt();
+		}
+	}
 
-    static void input() {
-        N = scan.nextInt();
-        K = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-    }
+	static void pro() {
+		int R = 0, rion_count = 0, ans = Integer.MAX_VALUE;
+		for (int L = 1; L <= n; L++) {
+			if (a[L - 1] == 1)
+				rion_count--;
+			while (R + 1 <= n && rion_count < k) {
+				if (a[++R] == 1)
+					rion_count++;
+			}
 
-    private static void pro() {
+			if (rion_count >= k) {
+				ans = Math.min(ans, R - L + 1);
+			}
+		}
 
-        int R = 0, count = 0, ans = N + 1;
-        for (int L = 1; L <= N; L++) {
-            if (A[L - 1] == 1) count--;
-            while (R + 1 <= N && count < K) {
-                if (A[++R] == 1) count++;
-            }
-            if (count == K) ans = Math.min(ans, R - L + 1);
-        }
-        if (ans <= N) System.out.println(ans);
-        else System.out.println(-1);
-    }
+		if (ans == Integer.MAX_VALUE)
+			ans = -1;
+		System.out.println(ans);
+	}
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+	static class FastScanner {
+		StringTokenizer st;
+		BufferedReader br;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
