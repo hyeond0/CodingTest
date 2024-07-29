ed9eb43942bd13ec1d7f9202becfbab50c18db4a
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-07-29 10:34:04 +0900

    [Silver IV] Title: 수들의 합 2, Time: 156 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,37 +1,62 @@
-
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static int[] a;
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
+	public static void main(String[] args) {
+		input();
+		pro();
+	}
 
-    static int n, m;
-    static int[] a;
+	static void input() {
+		n = scan.nextInt();
+		m = scan.nextInt();
+		a = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = scan.nextInt();
+		}
+	}
 
-    static void input() {
-        n = scan.nextInt();
-        m = scan.nextInt();
-        a = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            a[i] = scan.nextInt();
-        }
-    }
-    private static void pro() {
-        int R = 0, sum = 0, count = 0;
-        for (int L = 1; L <= n; L++) {
-            sum -= a[L-1];
-            while (R+1 <= n && sum < m) {
-                R++;
-                sum += a[R];
-            }
-            if (sum == m) count++;
-        }
-        System.out.println(count);
-    }
+	static void pro() {
+		int R = 0, sum = 0, ans = 0;
+		for (int L = 1; L <= n; L++) {
+			sum -= a[L - 1];
+			while (R + 1 <= n && sum < m) {
+				sum += a[++R];
+			}
+			if (sum == m) {
+				ans++;
+			}
+		}
 
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
+		System.out.println(ans);
+	}
+
+	static class FastScanner {
+		StringTokenizer st;
+		BufferedReader br;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
 }
