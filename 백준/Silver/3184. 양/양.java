8299d5fb80a359776b71579eec89b0b37c412a52
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-01 11:02:25 +0900

    [Silver I] Title: 양, Time: 180 ms, Memory: 22684 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.java
+++ 백준/Silver/3184. 양/양.java
@@ -2,75 +2,102 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M, liveSheep, liveWolf, sheepCount, wolfCount;
-    static String[] map;
-    static boolean[][] visit;
-    static StringBuilder sb = new StringBuilder();
-    static boolean sheepWin;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new String[N];
-        visit = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            map[i] = br.readLine();
-        }
-    }
+	static int R, C, wolf_count, sheep_count, wolf, sheep;
+	static String[] a;
+	static boolean[][] visit;
+	static int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 
-    private static void bfs(int x, int y) {
-        Queue<Integer> Q = new LinkedList<>();
-        sheepCount++;
-        Q.add(x);
-        Q.add(y);
-        visit[x][y] = true;
+	static StringBuilder sb = new StringBuilder();
+	static FastScanner scan = new FastScanner();
 
-        while (!Q.isEmpty()) {
-            x = Q.poll();
-            y = Q.poll();
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (visit[nx][ny]) continue;
-                if (map[nx].charAt(ny) == '#') continue;
-                Q.add(nx);
-                Q.add(ny);
-                visit[nx][ny] = true;
-                if (map[nx].charAt(ny) == 'o') sheepCount++;
-                else if (map[nx].charAt(ny) == 'v') wolfCount++;
-            }
-        }
+	public static void main(String[] args) {
+		input();
+		pro();
+		System.out.println(sb);
+	}
 
-        if (sheepCount > wolfCount) sheepWin = true;
-        else sheepWin = false;
-    }
+	static void input() {
+		R = scan.nextInt();
+		C = scan.nextInt();
+		a = new String[R];
+		for (int i = 0; i < R; i++) {
+			a[i] = scan.next();
+		}
+	}
 
-    private static void pro() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i].charAt(j) == 'v') liveWolf++;
-                else if (map[i].charAt(j) == 'o') {
-                    liveSheep++;
-                    if (!visit[i][j]) {
-                        sheepCount = 0;
-                        wolfCount = 0;
-                        bfs(i, j);
-                        if (sheepWin) liveWolf -= wolfCount;
-                        else liveSheep -= sheepCount;
-                    }
-                }
-            }
-        }
-        
-        sb.append(liveSheep).append(' ').append(liveWolf);
-        System.out.println(sb.toString());
-    }
+	static void pro() {
+		visit = new boolean[R][C];
+		wolf = 0;
+		sheep = 0;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (!visit[i][j] && a[i].charAt(j) != '#') {
+					dfs(i, j);
+					if (sheep > wolf) {
+						wolf = 0;
+					} else {
+						sheep = 0;
+					}
+					wolf_count += wolf;
+					sheep_count += sheep;
+					sheep = 0;
+					wolf = 0;
+				}
+			}
+		}
+		sb.append(sheep_count).append(' ').append(wolf_count);
+	}
 
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
+	static void dfs(int x, int y) {
+		if (a[x].charAt(y) == 'o')
+			sheep++;
+		if (a[x].charAt(y) == 'v')
+			wolf++;
+		visit[x][y] = true;
+		for (int k = 0; k < 4; k++) {
+			int nx = x + dir[k][0];
+			int ny = y + dir[k][1];
+			if (nx < 0 || ny < 0 || nx >= R || ny >= C)
+				continue;
+			if (visit[nx][ny])
+				continue;
+			if (a[nx].charAt(ny) == '#')
+				continue;
+			dfs(nx, ny);
+		}
+	}
+
+	static class FastScanner {
+		StringTokenizer st;
+		BufferedReader br;
+
+		FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
 }
