2a08f8398895306e62b7658ec1c170d1f60944c2
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-05 15:34:59 +0900

    [Silver I] Title: 현명한 나이트, Time: 352 ms, Memory: 37944 KB -BaekjoonHub

--- 백준/Silver/18404. 현명한 나이트/현명한 나이트.java
+++ 백준/Silver/18404. 현명한 나이트/현명한 나이트.java
@@ -1,52 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, knightX, knightY;
+    static int N, M, X, Y;
     static int[][] dist;
-    static int[][] dir = {{-2, 1}, {-2, -1}, {-1, -2}, {-1,2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-    static Scanner scan = new Scanner(System.in);
+    static List<Integer> x_list;
+    static List<Integer> y_list;
+    static int[][] dir = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    static FastScanner scan = new FastScanner();
     static StringBuilder sb = new StringBuilder();
-    static Queue<Integer> enemyQ = new LinkedList<>();
 
-    private static void input() {
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
-        knightX = scan.nextInt() - 1;
-        knightY = scan.nextInt() - 1;
-        dist = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        X = scan.nextInt();
+        Y = scan.nextInt();
+        x_list = new ArrayList<>();
+        y_list = new ArrayList<>();
+        for (int i = 1; i <= M; i++) {
+            x_list.add(scan.nextInt());
+            y_list.add(scan.nextInt());
+        }
+        dist = new int[N + 1][N + 1];
+    }
+
+    static void bfs(int x, int y) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 dist[i][j] = -1;
             }
         }
-        for (int i = 0; i < M; i++) {
-            int x = scan.nextInt() - 1, y = scan.nextInt() - 1;
-            enemyQ.add(x);
-            enemyQ.add(y);
-        }
-    }
-
-    private static void pro() {
-
-        bfs(knightX, knightY);
-
-        for (int i = 0; i < M; i++) {
-            int x = enemyQ.poll();
-            int y = enemyQ.poll();
-            sb.append(dist[x][y]).append(' ');
-        }
-
-        System.out.println(sb.toString());
-    }
-
-    private static void bfs(int x, int y) {
         Queue<Integer> Q = new LinkedList<>();
         Q.add(x);
         Q.add(y);
         dist[x][y] = 0;
-        int count = M;
 
         while (!Q.isEmpty()) {
             x = Q.poll();
@@ -54,8 +52,12 @@ public class Main {
             for (int k = 0; k < 8; k++) {
                 int nx = x + dir[k][0];
                 int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (dist[nx][ny] != -1) continue;
+                if (nx < 1 || ny < 1 || nx > N || ny > N) {
+                    continue;
+                }
+                if (dist[nx][ny] != -1) {
+                    continue;
+                }
                 Q.add(nx);
                 Q.add(ny);
                 dist[nx][ny] = dist[x][y] + 1;
@@ -63,8 +65,46 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        input();
-        pro();
+    static void pro() {
+        bfs(X, Y);
+        for (int i = 0; i < M; i++) {
+            sb.append(dist[x_list.get(i)][y_list.get(i)]).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+
+    static class FastScanner {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
