1a0f35650e038a52b755403df4c1c407cb6bece4
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-05 14:47:29 +0900

    [Silver I] Title: 나이트의 이동, Time: 484 ms, Memory: 144088 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,67 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int T, N, startX, startY, finishX, finishY;
+    static int I, start_x, start_y, end_x, end_y;
     static int[][] dist;
-    static int[][] dir = {{-2, 1}, {-2, -1}, {-1, -2}, {-1,2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-    static Scanner scan = new Scanner(System.in);
-    static StringBuilder sb = new StringBuilder();
-    private static void input() {
-        N = scan.nextInt();
-        dist = new int[N][N];
-        startX = scan.nextInt();
-        startY = scan.nextInt();
-        finishX = scan.nextInt();
-        finishY = scan.nextInt();
-    }
-
-    private static void bfs(int x, int y) {
-        Queue<Integer> Q = new LinkedList<>();
-        Q.add(x);
-        Q.add(y);
-        dist[x][y] = 0;
-        while(!Q.isEmpty()) {
-            x = Q.poll();
-            y = Q.poll();
-            for (int k = 0; k < 8; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (dist[nx][ny] > 0) continue;
-                Q.add(nx);
-                Q.add(ny);
-                dist[nx][ny] = dist[x][y] + 1;
-                if (nx == finishX && ny == finishY) break;
-            }
-        }
-    }
-
-    private static void pro() {
-        //dist 초기화
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                dist[i][j] = -1;
-            }
-        }
-
-        // map 중 시작점 탐색
-        // bfs
-        if (startX == finishX && startY == finishY) {
-            sb.append(0).append('\n');
-        } else {
-            bfs(startX, startY);
-            sb.append(dist[finishX][finishY]).append('\n');
-        }
-    }
+    static int[][] dir = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
+    static FastScanner scan = new FastScanner();
 
     public static void main(String[] args) {
-        T = scan.nextInt();
-        for (int i = 0; i < T; i++) {
+        int T = scan.nextInt();
+        for (int i = 1; i <= T; i++) {
             input();
             pro();
         }
-        System.out.println(sb.toString());
+    }
+
+    static void pro() {
+        bfs(start_x, start_y);
+        System.out.println(dist[end_x][end_y]);
+    }
+
+    static void bfs(int x, int y) {
+        for (int i = 0; i < I; i++) {
+            for (int j = 0; j < I; j++) {
+                dist[i][j] = -1;
+            }
+        }
+        Queue<Integer> que = new LinkedList<>();
+        que.add(x);
+        que.add(y);
+        dist[x][y] = 0;
+
+        while (!que.isEmpty()) {
+            x = que.poll();
+            y = que.poll();
+            for (int k = 0; k < 8; k++) {
+                int nx = x + dir[k][0];
+                int ny = y + dir[k][1];
+                if (nx < 0 || ny < 0 || nx >= I || ny >= I) {
+                    continue;
+                }
+                if (dist[nx][ny] != -1) {
+                    continue;
+                }
+                que.add(nx);
+                que.add(ny);
+                dist[nx][ny] = dist[x][y] + 1;
+            }
+        }
+    }
+
+    static void input() {
+        I = scan.nextInt();
+        dist = new int[I][I];
+        start_x = scan.nextInt();
+        start_y = scan.nextInt();
+        end_x = scan.nextInt();
+        end_y = scan.nextInt();
+    }
+
+    static class FastScanner {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
