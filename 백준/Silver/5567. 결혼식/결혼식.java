9844afcf6bf20a1a2b695dc58d14e92c19ee4205
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-06 09:46:39 +0900

    [Silver II] Title: 결혼식, Time: 172 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,54 +1,104 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
     static ArrayList<Integer>[] adj;
     static int[] dist;
 
-    private static void input() {
-        Scanner scan = new Scanner(System.in);
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
-        adj = new ArrayList[N+1];
+        adj = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
-        for (int i = 0; i < M; i++) {
-            int x = scan.nextInt(), y = scan.nextInt();
+
+        for (int i = 1; i <= M; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
             adj[x].add(y);
             adj[y].add(x);
         }
-        dist = new int[N+1];
     }
 
-    private static void bfs(int x) {
+    static void bfs(int start) {
+        // dist 초기화
+        for (int i = 1; i <= N; i++) {
+            dist[i] = -1;
+        }
         Queue<Integer> Q = new LinkedList<>();
-        Q.add(x);
+        Q.add(start);
+        dist[start] = 0;
 
         while (!Q.isEmpty()) {
-            x = Q.poll();
+            int x = Q.poll();
             for (int y : adj[x]) {
-                if (dist[y] != 0) continue;
+                if (dist[y] != -1) {
+                    continue;
+                }
                 Q.add(y);
                 dist[y] = dist[x] + 1;
             }
         }
     }
 
-    private static void pro() {
+
+    static void pro() {
+        dist = new int[N + 1];
+        int ans = 0;
         bfs(1);
-        int count = 0;
-        for (int i = 2; i <= N; i++) {
-            if (dist[i] == 1 || dist[i] == 2) count++;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] > 0 && dist[i] <= 2) {
+                ans++;
+            }
         }
-        System.out.println(count);
+        System.out.println(ans);
     }
 
-    public static void main(String[] args) {
-        input();
-        pro();
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
