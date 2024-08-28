d1dd8922818d6544d7d6c54d40ac1c0b7c3a6d42
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-28 10:56:35 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45472 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,21 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static FastReader scan = new FastReader();
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        Queue<Integer> queue = new LinkedList<>();
+        N = scan.nextInt();
+        Queue<Integer> Q = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            Q.add(i);
         }
 
-        while(queue.size() > 1) {
-            queue.poll();
-            queue.add(queue.poll());
+        while (Q.size() > 1) {
+            Q.poll();
+            Q.add(Q.poll());
         }
-        System.out.println(queue.poll());
+        System.out.println(Q.poll());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
