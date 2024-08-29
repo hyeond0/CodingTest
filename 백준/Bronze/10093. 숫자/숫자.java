e2b2ed67552e462b47e4b9213a60a1221d71c4fe
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-29 11:08:26 +0900

    [Bronze II] Title: 숫자, Time: 120 ms, Memory: 16028 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        if (A == B) {
+            System.out.println(0);
+            System.exit(0);
+        }
+        if (A > B) {
+            int tmp = A;
+            A = B;
+            B = tmp;
+        }
+        sb.append(B - A - 1).append('\n');
+        while (++A < B) {
+            sb.append(A).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
