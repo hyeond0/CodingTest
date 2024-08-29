6ea45fb5aee46e9018ef6a721161145d1f07fece
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-29 14:19:41 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        String s = scan.nextLine();
+        char[] cArray = s.toCharArray();
+        int[] a = new int[27];
+        for (char c : cArray) {
+            a[c - '0' - 48]++;
+        }
+        for (int i = 1; i <= 26; i++) {
+            sb.append(a[i]).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
