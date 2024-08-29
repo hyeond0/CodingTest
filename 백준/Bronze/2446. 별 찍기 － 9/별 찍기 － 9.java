1f284ad97cf348e35f80e43d1cd3a0c468bdb73f
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-29 14:01:42 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        int N = scan.nextInt();
+        for (int i = 2 * N - 1; i >= 1; i -= 2) {
+            for (int j = 1; j <= (2 * N - 1) - i; j += 2) {
+                sb.append(' ');
+            }
+            for (int k = 1; k <= i; k++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+        for (int i = 3; i <= 2 * N - 1; i += 2) {
+            for (int j = 1; j <= (2 * N - 1) - i; j += 2) {
+                sb.append(' ');
+            }
+            for (int k = 1; k <= i; k++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
