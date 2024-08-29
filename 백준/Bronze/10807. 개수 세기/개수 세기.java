54679e52eb05583296d5b7b96deb6ab44f918a94
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-29 15:04:05 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        int[] A = new int[201];
+        int N = scan.nextInt();
+        for (int i = 0; i < N; i++) {
+            A[scan.nextInt() + 100]++;
+        }
+        int V = scan.nextInt();
+        System.out.println(A[V + 100]);
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
