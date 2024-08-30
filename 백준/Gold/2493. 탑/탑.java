05414887d0637b04794d2b3f35c621e02b9cc816
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-30 10:43:04 +0900

    [Gold V] Title: 탑, Time: 2540 ms, Memory: 157336 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,39 +6,36 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        ArrayList<Integer> answerArray = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
         Stack<Integer> index = new Stack<>();
-        
-        // 제일 왼쪽 타워는 0
         stack.add(Integer.parseInt(st.nextToken()));
-        answerArray.add(0);
+        arrayList.add(0);
         index.add(1);
 
         for (int i = 1; i < n; i++) {
-            boolean isNone = true; // 더 높은 타워가 없는지 Check
-            int num = Integer.parseInt(st.nextToken()); 
+            boolean isNone = true;
+            int num = Integer.parseInt(st.nextToken());
             while (!stack.isEmpty()) {
-                if (num > stack.peek()) { // 왼쪽 타워가 높이가 더 낮은 타워라면
+                if (num > stack.peek()) {
                     stack.pop();
                     index.pop();
-                } else { // 높이가 더 높은 타워를 찾았다면
+                } else {
                     isNone = false;
-                    answerArray.add(index.peek());
+                    if (!index.isEmpty()) arrayList.add(index.peek());
                     break;
                 }
             }
-            if (isNone) { // 더 높은 타워가 없다면
-                answerArray.add(0);
+            if (isNone) {
+                arrayList.add(0);
 
             }
             stack.add(num);
             index.add(i+1);
         }
 
-        for (int i = 0; i< answerArray.size(); i++) {
-            System.out.print(answerArray.get(i) + " ");
+        for (int i = 0; i< arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + " ");
         }
     }
 }
