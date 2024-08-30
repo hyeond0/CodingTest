37c8bbe49f6986849b4c36a4dbaef87dcf6bd004
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-08-30 11:04:34 +0900

    [Gold V] Title: 탑, Time: 660 ms, Memory: 90172 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,41 +1,86 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        Stack<Integer> stack = new Stack<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        Stack<Integer> index = new Stack<>();
-        stack.add(Integer.parseInt(st.nextToken()));
-        arrayList.add(0);
-        index.add(1);
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < n; i++) {
-            boolean isNone = true;
-            int num = Integer.parseInt(st.nextToken());
-            while (!stack.isEmpty()) {
-                if (num > stack.peek()) {
-                    stack.pop();
-                    index.pop();
+    public static void main(String[] args) {
+        int N = scan.nextInt();
+        Stack<Top> stack = new Stack<>();
+        Stack<Top> stack2 = new Stack<>();
+        int[] A = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            stack.push(new Top(i, scan.nextInt()));
+        }
+        stack2.push(stack.pop());
+        while (!stack.isEmpty()) {
+            Top pop = stack.pop();
+            while (!stack2.isEmpty()) {
+                Top peek = stack2.peek();
+                if (pop.value > peek.value) {
+                    A[peek.idx] = pop.idx;
+                    stack2.pop();
                 } else {
-                    isNone = false;
-                    if (!index.isEmpty()) arrayList.add(index.peek());
                     break;
                 }
             }
-            if (isNone) {
-                arrayList.add(0);
+            stack2.push(pop);
+        }
+        for (int i = 1; i <= N; i++) {
+            sb.append(A[i]).append(' ');
+        }
+        System.out.println(sb);
+    }
 
-            }
-            stack.add(num);
-            index.add(i+1);
+    static class Top {
+        private final int idx;
+        private final int value;
+
+        public Top(int idx, int value) {
+            this.idx = idx;
+            this.value = value;
+        }
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        for (int i = 0; i< arrayList.size(); i++) {
-            System.out.print(arrayList.get(i) + " ");
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
