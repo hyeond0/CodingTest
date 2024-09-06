1b1d3d9acb6d89670d1b1c547cf70cf2099b7d92
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-06 23:05:37 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 208 ms, Memory: 68364 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static char[][] board;
+    static StringBuilder sb = new StringBuilder();
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        N = scan.nextInt();
+        board = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = ' ';
+            }
+        }
+
+        rec_func(N, 0, 0);
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(board[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void rec_func(int n, int x, int y) {
+        if (n == 1) {
+            board[x][y] = '*';
+            return;
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                rec_func(n / 3, x + n / 3 * i, y + n / 3 * j);
+            }
+        }
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
