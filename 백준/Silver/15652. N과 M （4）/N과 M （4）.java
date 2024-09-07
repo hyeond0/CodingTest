6f5e2d7f47e6a013d06c80f1493f02e7dbf40548
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-07 11:23:59 +0900

    [Silver III] Title: N과 M (4), Time: 88 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,39 +1,71 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[] selected;
-	static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		input();
-		rec_func(1);
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) {
+        n = scan.nextInt();
+        m = scan.nextInt();
+        arr = new int[n + 1];
+        rec_func(0);
+        System.out.println(sb);
+    }
 
-	static void input() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		selected = new int[M + 1];
-	}
+    static void rec_func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        int st = 1;
+        if (k != 0) {
+            st = arr[k - 1];
+        }
+        for (int i = st; i <= n; i++) {
+            arr[k] = i;
+            rec_func(k + 1);
+        }
+    }
 
-	static void rec_func(int k) throws Exception {
-		if (k == M + 1) {
-			for (int i = 1; i <= M; i++) {
-				sb.append(selected[i]).append(' ');
-			}
-			sb.append('\n');
-		} else {
-			int start = selected[k-1];
-			if (start == 0) start = 1;
-			for (int cand = start; cand <= N; cand++) {
-				selected[k] = cand;
-				rec_func(k + 1);
-				selected[k] = 0;
-			}
-		}
-	}
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
