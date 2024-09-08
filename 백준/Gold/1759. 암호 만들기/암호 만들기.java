b174d3b8bfacdcf86efd7d6b3a382c24c207b09e
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-09 08:24:25 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,67 +1,94 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int L, C;
-    static int[] selected;
-    static char[] alphabet;
+    static String[] arr, selected;
+    static boolean[] isUsed;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
         input();
-        ref_func(1);
-        System.out.println(sb.toString());
+        Arrays.sort(arr);
+        rec_func(0, 0);
+        System.out.println(sb);
     }
 
-    private static void ref_func(int k) {
-        if (k == L + 1) {
-            boolean isRight = checkPassword();
+    static void input() {
+        L = scan.nextInt();
+        C = scan.nextInt();
+        arr = new String[C];
+        selected = new String[C];
+        isUsed = new boolean[C];
+        for (int i = 0; i < C; i++) {
+            arr[i] = scan.next();
         }
-        else {
-            int start = selected[k-1];
-            for (int cand = start+1; cand <= C; cand++) {
-                boolean isUsed = false;
-                for (int i = 1; i < k; i++) {
-                    if (selected[i] == cand) isUsed = true;
+    }
+
+    static void rec_func(int k, int st) {
+        if (k == L) {
+            if (password_check()) {
+                for (int i = 0; i < L; i++) {
+                    sb.append(selected[i]);
                 }
-                if(!isUsed) {
-                    selected[k] = cand;
-                    ref_func(k+1);
-                    selected[k] = 0;
-                }
+                sb.append('\n');
+            }
+        }
+        for (int i = st; i < C; i++) {
+            if (!isUsed[i]) {
+                selected[k] = arr[i];
+                isUsed[i] = true;
+                rec_func(k + 1, i + 1);
+                isUsed[i] = false;
             }
         }
     }
 
-    private static boolean checkPassword() {
-        StringBuilder temp = new StringBuilder();
-        int moCount = 0;
-        for (int i = 1; i <= L; i++) {
-            char alpha = alphabet[selected[i]];
-            temp.append(alpha);
-            if (alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u') {
-                moCount++;
+    static boolean password_check() {
+        int mo_count = 0;
+        int ja_count = 0;
+        for (int i = 0; i < L; i++) {
+            switch (selected[i]) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                    mo_count++;
+                    break;
+                default:
+                    ja_count++;
+                    break;
             }
         }
-        if (moCount < L-1 && moCount != 0) {
-            sb.append(temp);
-            sb.append('\n');
-            return true;
-        }
-        else return false;
+        return mo_count >= 1 && ja_count >= 2;
     }
 
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        selected = new int[L+1];
-        alphabet = new char[C+1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= C; i++) {
-            alphabet[i] = st.nextToken().charAt(0);
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
-        Arrays.sort(alphabet);
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
