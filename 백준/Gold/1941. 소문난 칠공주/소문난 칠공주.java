6d9c05b88b24e21c22449706cc31991fc54fc7c1
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-09 09:59:32 +0900

    [Gold III] Title: 소문난 칠공주, Time: 520 ms, Memory: 217704 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,88 +1,105 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static String[] map = new String[5];
-    static int[][] choiceMap = new int[5][5];
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    static int[] selected = new int[8];
-    static int answer;
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 5; i++) {
-            map[i] = br.readLine();
-        }
+    static int N, ans;
+    static char[][] arr;
+    static int[] selected;
+    static boolean[] isUsed;
+    static int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        N = 5;
+        input();
+        rec_func(0, 0);
+        System.out.println(ans);
     }
-    private static void ref_func(int k) {
-        if (k == 8) { // 1 - 25 중 7개를 뽑았다면
-            int[] index = findIndex(); // 7개 숫자를 map 인덱스로 찾아가 choiceMap에 선택 처리
-            bfs(index[0], index[1]); // 맨 처음 선택된 숫자로 bfs 탐색 시작
-        } else {
-            int start = selected[k - 1];
-            for (int cand = start+1; cand <= 25; cand++) {
-                selected[k] = cand;
-                ref_func(k+1);
-                selected[k] = 0;
+
+    static void input() {
+        arr = new char[N][N];
+        isUsed = new boolean[25];
+        selected = new int[7];
+        for (int i = 0; i < N; i++) {
+            String s = scan.next();
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = s.charAt(j);
             }
         }
-
     }
-    private static void bfs(int x, int y) {
-        int sCount = 0; // 7개 정점 중 이다솜파 카운트
-        int linkCount = 1; // 7개 정점이 간선으로 연결되어 있는지 카운트
 
+    static void rec_func(int k, int st) {
+        if (k == 7) {
+            checkDasom();
+            return;
+        }
+        for (int i = st; i < 25; i++) {
+            selected[k] = i;
+            rec_func(k + 1, i + 1);
+        }
+    }
+
+    static void checkDasom() {
         Queue<Integer> Q = new LinkedList<>();
-        Q.add(x);
-        Q.add(y);
-        choiceMap[x][y] = 1; // 뽑힌 7개 정점 중에서도 탐색을 한 정점은 1
-        if (map[x].charAt(y) == 'S') sCount++; // 이다솜파인지 확인
-
+        int dasom = 0, adj = 0;
+        boolean[][] isp7 = new boolean[5][5];
+        boolean[][] vis = new boolean[5][5];
+        for (int i = 0; i < 7; i++) {
+            int x = selected[i] / 5, y = selected[i] % 5;
+            isp7[x][y] = true;
+            if (Q.isEmpty()) {
+                Q.add(x);
+                Q.add(y);
+                vis[x][y] = true;
+            }
+        }
         while (!Q.isEmpty()) {
-            x = Q.poll();
-            y = Q.poll();
-            for (int k = 0; k < 4; k++) { // 격자형 그래프 간선 확인
+            int x = Q.poll();
+            int y = Q.poll();
+            adj++;
+            dasom += arr[x][y] == 'S' ? 1 : 0;
+            for (int k = 0; k < 4; k++) {
                 int nx = x + dir[k][0];
                 int ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue; // map 밖으로 나가는지
-                if (choiceMap[nx][ny] != 0) continue; // 뽑은 7개가 아니거나 이미 탐색을 했는지
+                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || vis[nx][ny] || !isp7[nx][ny]) {
+                    continue;
+                }
                 Q.add(nx);
                 Q.add(ny);
-                linkCount++;
-                choiceMap[nx][ny] = 1; // 탐색 여부 체크
-                if (map[nx].charAt(ny) == 'S') sCount++; // 이다솜파인지 확인
+                vis[nx][ny] = true;
             }
         }
-        // 탐색이 끝난 뒤 7개 정점이 연결되어있고, 이다솜파가 4명 이상이라면
-        if (linkCount == 7 && sCount >= 4) answer++;
+        if (adj >= 7 && dasom >= 4) {
+            ans++;
+        }
+
     }
 
-    private static int[] findIndex() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                choiceMap[i][j] = -1; // choiceMap 초기화
-            }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        int[] ret = new int[2];
-        // 1 - 25 까지 중에서 뽑아온 7개를 2차원 배열 index에 맞게 저장.
-        for (int i = 1; i <= 7; i++) {
-            int selectedNumber = selected[i];
-            int x = (selectedNumber - 1) / 5;
-            int y = (selectedNumber - 1) % 5;
-            if (i == 1) {
-                ret[0] = x; // bfs 탐색을 위해 7개 중 맨 처음 뽑힌 정점 좌표를 x, y로 저장
-                ret[1] = y;
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            choiceMap[x][y] = 0; // 뽑아온 7개는 0, 나머지는 -1로 탐색 시작
+            return st.nextToken();
         }
-        return ret;
-    }
 
-    public static void main(String[] args) throws IOException {
-        input();
-        // 백트래킹을 통해 숫자 1 - 25 중 7개 뽑아서 bfs 돌리기
-        ref_func(1);
-        // 칠공주 결성 경우의 수 출력
-        System.out.println(answer);
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
