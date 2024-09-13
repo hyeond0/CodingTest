443a0a87a1c27a0b6bb5c8a2079dc7e225f069ff
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-13 16:04:52 +0900

    [Silver IV] Title: 카드, Time: 292 ms, Memory: 31692 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,47 +1,83 @@
-
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
+    static int n;
+    static long[] arr;
+    static FastReader scan = new FastReader();
 
-    static long[] card;
-    private static void input() {
-        Scanner scan = new Scanner(System.in);
-        N = scan.nextInt();
-        card = new long[N+1];
-        for (int i = 1; i <= N; i++) {
-            card[i] = scan.nextLong();
-        }
-    }
-
-    private static void pro() {
-        // Sort 정렬하기
-        Arrays.sort(card, 1, N+1);
-        // mode: 최빈값, modeCnt: 최빈값의 등장 횟수, curCnt: 현재 값(a[1])의 등장 횟수
-        long mode = card[1];
-        int modeCnt = 1, curCnt = 1;
-
-
-        // TODO
-        // 2번 원소부터 차례대로 보면서, 같은 숫자가 이어서 나오고 있는 지, 새로운 숫자가 나왔는 지를 판단하여
-        // curCnt를 갱신해주고, 최빈값을 갱신하는 작업.
-        for (int i = 2; i <= N; i++) {
-            if (card[i] == card[i-1]) {
-                curCnt++;
-                if (curCnt > modeCnt) {
-                    mode = card[i];
-                    modeCnt = curCnt;
-                }
-            } else {
-                curCnt = 1;
-            }
-        }
-        // 정답 출력하기
-        System.out.println(mode);
-    }
     public static void main(String[] args) {
         input();
         pro();
+    }
+
+    static void input() {
+        n = scan.nextInt();
+        arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextLong();
+        }
+    }
+
+    static void pro() {
+        Arrays.sort(arr);
+        int cnt = 0, maxCnt = 0;
+        long maxVal = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i] == arr[i - 1]) {
+                cnt++;
+            } else {
+                if (cnt > maxCnt) {
+                    maxCnt = cnt;
+                    maxVal = arr[i - 1];
+                }
+                cnt = 1;
+            }
+        }
+        if (cnt > maxCnt) {
+            maxVal = arr[n - 1];
+        }
+        System.out.println(maxVal);
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 }
