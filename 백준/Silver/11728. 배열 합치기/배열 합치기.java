8da83fbc8f7d460c23e0d6555729c8c705d5735d
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-13 14:32:54 +0900

    [Silver V] Title: 배열 합치기, Time: 1108 ms, Memory: 353992 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,54 +1,89 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static List<Integer> a = new ArrayList<>();
-	static StringBuilder sb = new StringBuilder();
-	static FastScanner scan = new FastScanner();
+    static int n, m;
+    static int[] a, b, c;
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		input();
-		pro();
-	}
+    public static void main(String[] args) {
+        input();
+        pro();
+        System.out.println(sb);
+    }
 
-	static void input() {
-		n = scan.nextInt();
-		m = scan.nextInt();
-		for (int i = 1; i <= n + m; i++) {
-			a.add(scan.nextInt());
-		}
-	}
+    static void pro() {
+        int a_idx = 0;
+        int b_idx = 0;
+        for (int i = 0; i < n + m; i++) {
+            if (a_idx >= n) {
+                c[i] = b[b_idx++];
+                continue;
+            }
+            if (b_idx >= m) {
+                c[i] = a[a_idx++];
+                continue;
+            }
+            if (a[a_idx] <= b[b_idx]) {
+                c[i] = a[a_idx++];
+            } else {
+                c[i] = b[b_idx++];
+            }
+        }
 
-	static void pro() {
-		Collections.sort(a);
-		for (int num : a) {
-			sb.append(num).append(' ');
-		}
-		System.out.println(sb.toString());
-	}
+        for (int i = 0; i < n + m; i++) {
+            sb.append(c[i]).append(' ');
+        }
+    }
 
-	static class FastScanner {
-		StringTokenizer st;
-		BufferedReader br;
+    static void input() {
+        n = scan.nextInt();
+        m = scan.nextInt();
+        a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scan.nextInt();
+        }
+        c = new int[n + m];
+    }
 
-		FastScanner() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-	}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
 }
