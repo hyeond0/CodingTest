dfc9870194bdbd42cd20c7f25952ec4070cc14bf
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-19 16:43:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,29 +1,45 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[] Dy;
     static int n;
-
-    private static void input() {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-    }
-
-    private static void pro() {
-        Dy = new int[1010];
-        Dy[1] = 1;
-        Dy[2] = 2;
-        Dy[3] = 3;
-        for (int i = 4; i <= 1000; i++) {
-            Dy[i] = (Dy[i-1] + Dy[i-2]) % 10007;
-        }
-        System.out.println(Dy[n]);
-    }
+    static int[] d;
+    static FastReader scan = new FastReader();
 
     public static void main(String[] args) {
-        input();
-        pro();
+        n = scan.nextInt();
+        d = new int[1001];
+        d[1] = 1;
+        d[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2]) % 10007;
+        }
+        System.out.println(d[n]);
+    }
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
