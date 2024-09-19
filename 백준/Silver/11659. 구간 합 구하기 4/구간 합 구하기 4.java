df3a7933c5a682f71f6c980550c4633c670d9c33
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-19 16:47:17 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 512 ms, Memory: 57816 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,38 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static FastReader sc = new FastReader();
+    static int n, m;
+    static int[] d, nums;
+    static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] S = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            int num = sc.nextInt();
-            S[i] = S[i - 1] + num;
+        n = scan.nextInt();
+        m = scan.nextInt();
+        d = new int[n + 1];
+        d[1] = scan.nextInt();
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1] + scan.nextInt();
         }
 
-        for (int k = 0; k < M; k++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            System.out.println(S[j] - S[i - 1]);
+        for (int i = 0; i < m; i++) {
+            int first = scan.nextInt();
+            int second = scan.nextInt();
+            sb.append(d[second] - d[first - 1]).append('\n');
         }
+        System.out.println(sb);
     }
 
     static class FastReader {
         StringTokenizer st;
         BufferedReader br;
 
-        public FastReader() {
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
-
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -43,16 +47,6 @@ public class Main {
 
         int nextInt() {
             return Integer.parseInt(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
 }
