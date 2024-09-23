7df00874512265a2e30779ad17edf5a032851ab9
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-23 10:44:47 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 92 ms, Memory: 13492 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] a, d, pre;
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        n = scan.nextInt();
+        a = new int[1005];
+        d = new int[1005];
+        pre = new int[1005];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (a[i] > a[j]) {
+                    if (d[j] + 1 > d[i]) {
+                        d[i] = d[j] + 1;
+                        pre[i] = j;
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (d[i] > max) {
+                sb.setLength(0);
+                max = d[i];
+                List<Integer> pres = new ArrayList<>();
+                sb.append(max).append('\n');
+                int cur = i;
+                while (cur != 0) {
+                    pres.add(cur);
+                    cur = pre[cur];
+                }
+                for (int j = pres.size() - 1; j >= 0; j--) {
+                    sb.append(a[pres.get(j)]).append(' ');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+
+
+    static class FastReader {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
