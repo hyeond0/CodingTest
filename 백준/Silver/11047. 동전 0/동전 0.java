ed8ce449e9ab5d4600a812e180dde472b67ad68e
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-24 15:52:09 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, k;
+    static int[] a;
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        n = scan.nextInt();
+        k = scan.nextInt();
+        a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
+        }
+        int ans = 0;
+        for (int i = n; i > 0; i--) {
+            if (k / a[i] > 0) {
+                ans += k / a[i];
+                k -= a[i] * (k / a[i]);
+            }
+        }
+        System.out.println(ans);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
