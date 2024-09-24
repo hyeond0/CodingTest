451810a4de9f0a4a625f3da31a05bf3e23dd0b6e
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-24 13:44:43 +0900

    [Silver III] Title: 01타일, Time: 88 ms, Memory: 19428 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static long[] d;
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        n = scan.nextInt();
+        d = new long[1000005];
+        d[1] = 1;
+        d[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2]) % 15746;
+        }
+        System.out.println(d[n]);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
