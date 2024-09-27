414024046265fc5a6893908953302d39ab5026ea
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-09-27 10:35:01 +0900

    [Bronze II] Title: 소수 찾기, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) {
+        n = scan.nextInt();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int num = scan.nextInt();
+            if (isPrime(num)) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
