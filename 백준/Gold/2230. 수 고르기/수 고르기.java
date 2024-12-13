c91b86b908e7afecf5580389de10d0aca845cc45
Jeong Hyundo <95916780+hyeond0@users.noreply.github.com>
2024-12-13 14:42:20 +0900

    [Gold V] Title: 수 고르기, Time: 348 ms, Memory: 34384 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,65 +1,64 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static int[] a;
-	static StringBuilder sb = new StringBuilder();
-	static FastScanner scan = new FastScanner();
+    static int n, m;
+    static int[] a;
+    static FastReader scan = new FastReader();
 
-	public static void main(String[] args) {
-		input();
-		pro();
-	}
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
 
-	static void input() {
-		n = scan.nextInt();
-		m = scan.nextInt();
-		a = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			a[i] = scan.nextInt();
-		}
-	}
+    static void input() {
+        n = scan.nextInt();
+        m = scan.nextInt();
+        a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+    }
 
-	static void pro() {
-		Arrays.sort(a);
-		int R = 1, ans = Integer.MAX_VALUE;
-		for (int L = 1; L <= n; L++) {
-			while (R + 1 <= n && Math.abs(a[R] - a[L]) < m) {
-				R++;
-			}
-			int value = Math.abs(a[R] - a[L]);
-			if (value >= m) {
-				ans = Math.min(value, ans);
-			}
-		}
-		if (ans == Integer.MAX_VALUE) {
-			ans = 0;
-		}
-		System.out.println(ans);
-	}
+    static void pro() {
+        Arrays.sort(a);
+        int en = 0, min = Integer.MAX_VALUE;
+        for (int st = 0; st < n; st++) {
+            while (en < n && a[en] - a[st] < m) {
+                en++;
+            }
+            if (en == n) {
+                break;
+            }
+            min = Math.min(min, a[en] - a[st]);
+        }
+        System.out.println(min);
+    }
 
-	static class FastScanner {
-		StringTokenizer st;
-		BufferedReader br;
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-		FastScanner() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-	}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
 }
